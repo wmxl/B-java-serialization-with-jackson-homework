58df2761b9b386ea224f75b20064c8873088fee9
wmxl <294036748@qq.com>
2020-11-06 22:10:32 +0800

    json transfer for events and persons

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventType.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventType.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum EventType {
   UPLOAD("U"), DOWNLOAD("D");
 
@@ -7,5 +9,21 @@ public enum EventType {
 
   EventType(String code) {
     this.code = code;
+  }
+
+//  这个不写也可以，写了可以自定义一些东西
+//  @JsonCreator
+//  public static EventType from(String code) {
+//    for (EventType et : values()) {
+//      if (et.code.equals(code)) {
+//        return et;
+//      }
+//    }
+//    return null;
+//  }
+
+  @JsonValue
+  public String getValue() {
+    return code;
   }
 }
--- src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
@@ -1,16 +1,29 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class PersonVo {
 
+  public PersonVo(String id, Integer age, String name, String hobby) {
+    this.id = id;
+    this.age = age;
+    this.name = name;
+    this.hobby = hobby;
+
+    if(age == null){
+      this.age = 0;
+    }
+  }
+
   private String id;
-  private Integer age;
+
+  private Integer age ;
   private String name;
+  @JsonInclude(JsonInclude.Include.NON_NULL)
   private String hobby;
 }
